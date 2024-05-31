240e62e16f788c3b37389fc28adf77e84d14aab4
eminbasbayan <emin.basbayan@icloud.com>
2024-05-31 10:16:06 +0300

    component içinde css dosyası çalıştırma

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,31 +1,15 @@
-const productItemCss = {
-  border: "1px solid #ccc",
-  width: "200px",
-};
-
-const productImageCss = {
-  width: "100%",
-  display: "flex",
-};
-
-const productInfoCss = {
-  padding: "14px",
-  display: "flex",
-  flexDirection: "column",
-  rowGap: "4px",
-};
+import "./ProductItem.css";
 
 function ProductItem() {
   return (
-    <div className="product-item" style={productItemCss}>
+    <div className="product-item">
       <div className="product-image">
         <img
           src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
           alt="şapka"
-          style={productImageCss}
         />
       </div>
-      <div className="product-info" style={productInfoCss}>
+      <div className="product-info">
         <strong>Şapka</strong>
         <span>500₺</span>
       </div>
