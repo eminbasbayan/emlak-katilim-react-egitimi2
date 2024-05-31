aa74fa135c64ab1fd277d058cbdebbcf97e0e77e
eminbasbayan <emin.basbayan@icloud.com>
2024-05-31 14:32:11 +0300

    button dinamik proptypes ayarlama

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Button from "./components/UI/Button";
 function App() {
   return (
     <div className="app">
-        <h1>App</h1>
-        <Button />
-        <Products />
+      <h1>App</h1>
+      <Button title="Add To Cart" size="xl" color="primary" />
+      <Button title="Delete" size="lg" color="danger" />
+      <Button title="Update" size="sm" color="success" />
+      <Button title="Get Products" size="xl" color="success" />
+      <Products />
     </div>
   );
 }
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,5 +1,16 @@
-function Button() {
-  return <button>Click</button>;
+import PropTypes from "prop-types";
+import "./Button.css";
+
+function Button(props) {
+  const classNames = `btn btn-${props.size} btn-${props.color}`;
+
+  return <button className={classNames}>{props.title}</button>;
 }
+
+Button.propTypes = {
+  size: PropTypes.oneOf(["xl", "lg", "sm"]).isRequired,
+  color: PropTypes.oneOf(["primary", "danger", "success"]),
+  title: PropTypes.string.isRequired,
+};
 
 export default Button;
