3fe84072d3a8fefe73e9c800279ed29e163d7963
eminbasbayan <emin.basbayan@icloud.com>
2024-05-31 10:11:39 +0300

    component inline css kullanımı

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,14 +1,31 @@
+const productItemCss = {
+  border: "1px solid #ccc",
+  width: "200px",
+};
+
+const productImageCss = {
+  width: "100%",
+  display: "flex",
+};
+
+const productInfoCss = {
+  padding: "14px",
+  display: "flex",
+  flexDirection: "column",
+  rowGap: "4px",
+};
+
 function ProductItem() {
   return (
-    <div className="product-item">
+    <div className="product-item" style={productItemCss}>
       <div className="product-image">
         <img
           src="https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
           alt="şapka"
-          width={200}
+          style={productImageCss}
         />
       </div>
-      <div className="product-info">
+      <div className="product-info" style={productInfoCss}>
         <strong>Şapka</strong>
         <span>500₺</span>
       </div>
