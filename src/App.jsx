4af6fdac95222987c78fbd596c60cb0041bb28bd
eminbasbayan <emin.basbayan@icloud.com>
2024-05-31 12:25:48 +0300

    button componenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Products from "./components/Products/Products";
+import Button from "./components/UI/Button";
 
 function App() {
   return (
     <div className="app">
         <h1>App</h1>
+        <Button />
         <Products />
     </div>
   );
