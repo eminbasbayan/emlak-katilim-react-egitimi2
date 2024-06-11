87e1def7f1116bd0c7b3d8b1e7c5a4ba03073ab4
eminbasbayan <emin.basbayan@icloud.com>
2024-06-11 14:13:20 +0300

    total cart items

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -4,9 +4,17 @@ import CartItem from "../components/Products/ProductItem";
 
 const CartPage = () => {
   const { cartItems } = useContext(CartContext);
+
+  const totalCartItems = cartItems.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+
   return (
     <div className="cart-page">
       <h1>Cart Page</h1>
+      <div className="total">
+        <strong>Total: </strong> {totalCartItems.toFixed(2)}
+      </div>
       {cartItems.length === 0 && <h3>Sepette hiç ürün yok!</h3>}
       <div className="products">
         {cartItems.map((item) => (
