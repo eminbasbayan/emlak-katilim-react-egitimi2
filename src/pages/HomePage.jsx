765f2a02d0c88b362da88f6ce77fe3e346bf2043
eminbasbayan <emin.basbayan@icloud.com>
2024-06-12 16:15:33 +0300

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,8 @@
-import Counter from "../Counter";
-
 const HomePage = () => {
 
   return (
     <div className="home-page">
       <h1>Home Page</h1>
-      <Counter />
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ea illum
         minima quos inventore possimus vitae magni nisi qui sed? Tenetur iusto
